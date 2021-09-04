cc587eea750db65919f1840f4bcb4663e5026d78
Keshav Singh <keshav@Keshavs-Mac-mini.local>
2021-09-04 18:14:33 +0530

    yup added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 import { Formik } from "formik";
+import * as Yup from "yup";
+
+var signinSchema = Yup.object({
+  username: Yup.string().required("Username is Required"),
+  password: Yup.string().required("Password is Required"),
+});
 
 function App() {
   return (
@@ -14,36 +20,52 @@ function App() {
           onSubmit={(data) => {
             console.log(data);
           }}
+          validationSchema={signinSchema}
         >
-          {({ values, handleChange, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <div className="form-field">
-                <label htmlFor="username"></label>
-                <input
-                  type="text"
-                  id="username"
-                  className="form-control"
-                  placeholder="Enter Username"
-                  onChange={handleChange}
-                  value={values.username}
-                />
-              </div>
-              <div className="form-field">
-                <label htmlFor="password"></label>
-                <input
-                  type="password"
-                  id="password"
-                  className="form-control"
-                  placeholder="Enter password"
-                  onChange={handleChange}
-                  value={values.password}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary mt-4">
-                Sign In
-              </button>
-            </form>
-          )}
+          {({
+            values,
+            handleChange,
+            handleSubmit,
+            errors,
+            handleBlur,
+            touched,
+          }) => {
+            console.log("errors", errors);
+            console.log("touched", touched);
+            return (
+              <form onSubmit={handleSubmit}>
+                <div className="form-field">
+                  <label htmlFor="username"></label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="form-control"
+                    placeholder="Enter Username"
+                    onChange={handleChange}
+                    value={values.username}
+                    onBlur={handleBlur}
+                  />
+                  <p>{touched.username && errors.username}</p>
+                </div>
+                <div className="form-field">
+                  <label htmlFor="password"></label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    onChange={handleChange}
+                    value={values.password}
+                    onBlur={handleBlur}
+                  />
+                  {touched.password && errors.password}
+                </div>
+                <button type="submit" className="btn btn-primary mt-4">
+                  Sign In
+                </button>
+              </form>
+            );
+          }}
         </Formik>
       </div>
     </div>
